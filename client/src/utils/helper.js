33dbc9082d925ed86aa0eafa5cb709a1a05d2667
Trần Bảo Long <long.dp2005@gmail.com>
2026-03-22 14:53:59 +0700

    reload content api Excel

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -236,7 +236,25 @@ export const exportExcel = async (
       params,
       responseType: "blob",
     });
+
+    const url = window.URL.createObjectURL(new Blob([res.data]));
+    const link = document.createElement("a");
+
+    link.href = url;
+    link.setAttribute("download", filename);
+
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+
+    toast.success("Xuất Excel thành công!");
+  } catch (error) {
+    const err = handleError(error, "Lỗi khi xuất Excel");
+    toast.error(err.message);
+    return err;
   }
+};
 
 //Api pdf
 export const exportPDF = async (endpoint, params = {}, filename = "file.pdf") => {
