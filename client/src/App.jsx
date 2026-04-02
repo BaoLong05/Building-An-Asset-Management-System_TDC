2bd1b355c070efbd31f174fcdebee48ef35f661c
BaoLong05 <long.dp2005@gmail.com>
2026-04-02 15:43:15 +0700

    done feat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { useRoutes } from "react-router-dom";
 import AdminRoutes from "./routes/AdminRoutes";
 import LoginRoute from "./routes/LoginRoute";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   const routes = useRoutes([
     AdminRoutes,
     LoginRoute,
+   { path: "*", element: <NotFound /> },
   ]);
   return routes;
 }
