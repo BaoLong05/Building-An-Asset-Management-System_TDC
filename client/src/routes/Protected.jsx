8d113fb9261e2c5108712e51c2601c9038592836
BaoLong05 <long.dp2005@gmail.com>
2026-04-06 10:11:02 +0700

    done feat

---
+++ client/src/routes/Protected.jsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from "react-router-dom";
+const ProtectedRoute = () =>{
+    const token = sessionStorage.getItem("token")
+    if(!token){
+        return <Navigate to= "/" replace/>
+    }
+    return <Outlet/>;
+}
+export default ProtectedRoute;
